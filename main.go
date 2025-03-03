da5fbf6ad2ee9b3d3a6c42ac54a07d7fecf9b1ce
mohammad javad <tvklqa@gmail.com>
2025-03-03 16:30:16 +0330

    checking confilig

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(" please enter second number:")
+	fmt.Println(" please2 enter second number:")
 	_, err = fmt.Scan(&num2)
 	if err != nil {
 		fmt.Println("Invalid input for second number.")
