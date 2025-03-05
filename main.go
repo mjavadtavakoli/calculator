abb27db0e76074682093e046ad60d69cb74a3e62
mohammad javad <tvklqa@gmail.com>
2025-03-05 08:30:02 +0330

    checking rebase

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	var num1, num2 float64
 	var operator string
 
-	fmt.Println("picky picky oh oh  enter first number:")
+	fmt.Println("javad jan enter first number:")
 
 	_, err := fmt.Scan(&num1)
 	if err != nil {
@@ -17,14 +17,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(" enter operator (+, -, *, /):")
+	fmt.Println("plase  enter operator (+, -, *, /):")
 	_, err = fmt.Scan(&operator)
 	if err != nil {
 		fmt.Println("Invalid input for operator.")
 		os.Exit(1)
 	}
 
-	fmt.Println("  enter second number:")
+	fmt.Println(" hadi ziba enter second number:")
 	_, err = fmt.Scan(&num2)
 	if err != nil {
 		fmt.Println("Invalid input for second number.")
